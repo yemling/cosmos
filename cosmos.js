32373f1ce8b9a3892e0ecaa48d420bc03bbf8cce
Gemma Nash <gemma.pumpr@hotmail.co.uk>
2018-06-01 17:13:29 +0100

    Making ready for deployment
    Improvements to dynamic manipulation.
    Inserting the iamges dynamically. What to do if they don't exist?

--- cosmos.js
+++ cosmos.js
@@ -1,4 +1,27 @@
 var cosmos = {};
+cosmos.lcmClasses = [
+	{id:"LCM_CLASS_1", description:"% of Broadleaf Woodland (LCM 2015 Class 1)"},
+	{id:"LCM_CLASS_2", description:"% of Coniferous Woodland (LCM 2015  Class 2)"},
+	{id:"LCM_CLASS_3", description:"% of Arable and horticulture (LCM 2015 Class 3)"},
+	{id:"LCM_CLASS_4", description:"% of Improved Grassland (LCM 2015  Class 4)"},
+	{id:"LCM_CLASS_5", description:"% of Neutral Grassland (LCM 2015  Class 5)"},
+	{id:"LCM_CLASS_6", description:"% of Calcareous Grassland (LCM 2015  Class 6)"},
+	{id:"LCM_CLASS_7", description:"% of Acid grassland (LCM 2015  Class 7)"},
+	{id:"LCM_CLASS_8", description:"% of Fen, marsh and swamp (LCM 2015  Class 8)"},
+	{id:"LCM_CLASS_9", description:"% of Heather (LCM 2015 Class 9)"},
+	{id:"LCM_CLASS_10", description:"% of Heather Grassland (LCM 2015  Class 10)"},
+	{id:"LCM_CLASS_11", description:"% of Bog (LCM 2015  Class 11)"},
+	{id:"LCM_CLASS_12", description:"% of Inland Rock (LCM 2015  Class 12)"},
+	{id:"LCM_CLASS_13", description:"% of Saltwater (LCM 2015  Class 13)"},
+	{id:"LCM_CLASS_14", description:"% of Freshwater (LCM 2015  Class 14)"},
+	{id:"LCM_CLASS_15", description:"% of Supra-littoral rock (LCM 2015  Class 15)"},
+	{id:"LCM_CLASS_16", description:"% of Supra-littoral sediment (LCM 2015  Class 16)"},
+	{id:"LCM_CLASS_17", description:"% of Littoral rock (LCM 2015  Class 17)"},
+	{id:"LCM_CLASS_18", description:"% of Littoral sediment (LCM 2015  Class 18)"},
+	{id:"LCM_CLASS_19", description:"% of Saltmarsh (LCM 2015  Class 19)"},
+	{id:"LCM_CLASS_20", description:"% of Urban (LCM 2015  Class 20)"},
+	{id:"LCM_CLASS_21", description:"% of Suburban (LCM 2015  Class 21)"}
+];
 cosmos.graphTypes = [
 	{
 		tab:"Summary",
@@ -66,23 +89,35 @@ cosmos.graphTypes = [
 	}
 ];
 cosmos.graphParams = {};
-cosmos.graphParams.height = 500;
-cosmos.graphParams.width = 800;
+cosmos.graphParams.height = 700;
+cosmos.graphParams.width = 1000;
 cosmos.graphParams.days = 50;
 cosmos.graphParams.endOffset = 0;
 cosmos.graphParams.startDate = 0;
 cosmos.graphParams.endDate = 30;
 cosmos.graphParams.urlType = 'days';
-cosmos.station = 'ALIC1';
+cosmos.station = 'CHOBH';
 
 cosmos.init = function()
 {
+	$('#jsPages').load('station.htm',cosmos.structurePage);
+	return;
+}
+cosmos.structurePage = function()
+{
+	wrUtils.pageLoading();
 	//jquery ui tabs for the details, graphs and maps tabs
+	$('ul#stationList li').click(function()
+	{
+		cosmos.station = $(this).text();
+		cosmos.getStationData();
+	});
 	$("#stationTabs").tabs();
 	$("#parameterTabs").tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
     $("#parameterTabs li").removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
 	$('#graphsTabLink').click(cosmos.drawGraphs);
 	cosmos.drawGraphContainer();
+	cosmos.outputImages();
 	//add a magnifying glass on the lightbox images
 	$('figure').prepend('<i class="fa fa-search-plus"></i>');
 	//featherlight doesn't add captions to images by default, so extra code here to add it from the alt text.
@@ -121,6 +156,13 @@ cosmos.init = function()
 			cosmos.graphParams.urlType = 'days';
 		}                
 	});
+	cosmos.getStationData();
+	cosmos.drawMap();
+	return;
+}
+cosmos.drawMonthSlider = function()
+{
+	wrUtils.dates = wrUtils.getSpiDates();
 	var monthStartHandle = $( "#monthStart-handle" );
 	var monthEndHandle = $( "#monthEnd-handle" );
 	$( "#graphDates" ).slider({ 
@@ -145,12 +187,11 @@ cosmos.init = function()
 	});
 	$( ".monthStart" ).text(wrUtils.formatDate(0).shortDate);
 	$( ".monthEnd" ).text(wrUtils.formatDate(wrUtils.mapDates.length-1).shortDate);
-	cosmos.getStationData();
 	return;
 }
 cosmos.getStationData = function()
 {
-	$.getJSON('data/cosmos-query-data.json',cosmos.outputStationData);
+	$.getJSON('data/cosmos-query-data_'+cosmos.station+'.json',cosmos.outputStationData);
 }
 cosmos.outputStationData = function(stationData)
 {
@@ -159,23 +200,59 @@ cosmos.outputStationData = function(stationData)
 	{
 		calibrated = 'Yes';
 	}
+	wrUtils.startYear = stationData["date-installed"].split('-')[0];
+	var yearDecommissioned = stationData["date-decommissioned"].split('-')[0];
+	var currentYear = (new Date).getFullYear();
+	if(yearDecommissioned < currentYear)
+	{
+		wrUtils.endYear = yearDecommissioned;
+	}
+	cosmos.drawMonthSlider();
 	$('.siteName').text(stationData["site-name"]);
 	$('#bng').text(stationData.easting+', '+stationData.northing);
 	$('#latLng').text(stationData.latitude+', '+stationData.longitude);
-	$('#region').text(stationData["site-metadata"]["nuts1-region"]);
-	$('#lcm').text(stationData["site-metadata"]["bhab"]);
-	$('#host').text(stationData["site-metadata"]["host-dclass"]);
-	$('#superficalGeologyClass').text(wrUtils.autocase(stationData["site-metadata"]["rcs-d"]));
-	$('#soilType').text(wrUtils.autocase(stationData["site-metadata"]["soil-type"]));
-	$('#soilDepth').text(wrUtils.autocase(stationData["site-metadata"]["soil-depth"]));
-	$('#bulkDensity').text(stationData["calibration-info"]["ref-bulk-density"]);
-	$('#latticeWater').text(stationData["calibration-info"]["ref-lattice-water"]);
-	$('#soilOrganicCarbon').text(stationData["calibration-info"]["ref-soil-organic-carbon"]);
-	$('#calibrated').text(calibrated);
-	$('#rainfall').text(stationData["site-metadata"]["saar"]);
-	cosmos.drawMap(stationData);	
+	$('#region').text(stationData["region"]);
+	$('#siteDesc').text(stationData["description"]);
+	$('#lcm').text(stationData["land-cover"]);
+	$('#altitude').text(stationData["altitude"]);
+	$('#superficalGeologyClass').text(wrUtils.autocase(stationData["bedrock-class"]));
+	$('#soilTexture').text(wrUtils.autocase(stationData["soil-texture"]));
+	$('#soilThickness').text(wrUtils.autocase(stationData["soil-thickness"]));
+	$('#rainfall').text(stationData["saar"]);
+	if(typeof stationData["calibration-info"] != 'undefined')
+	{
+		$('#bulkDensity').text(stationData["calibration-info"]["ref-bulk-density"]);
+		$('#latticeWater').text(stationData["calibration-info"]["ref-lattice-water"]);
+		$('#soilOrganicCarbon').text(stationData["calibration-info"]["ref-soil-organic-carbon"]);
+		$('#calibrated').text(calibrated);
+	}
+	if(typeof stationData["site-attributes"] != 'undefined')
+	{
+		var footprint = '';
+		for(var c = 0; c < cosmos.lcmClasses.length;c++)
+		{
+			var lcmClass = cosmos.lcmClasses[c];
+			if(typeof stationData["site-attributes"][lcmClass.id] != 'undefined')
+			{
+				footprint += stationData["site-attributes"][lcmClass.id]+lcmClass.description+'<br />';
+			}
+		}
+		$('#footprint').html(footprint);
+	}
+	cosmos.updateMap(stationData);
 }
 cosmos.drawMap = function(stationData)
+{
+	cosmos.map = L.map('mapid');
+	cosmos.osMap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		id: 'mapbox.streets',
+		accessToken: 'pk.eyJ1IjoiZ2VtbWF2bmFzaCIsImEiOiJjamdwNjR5YzMwZTc5MndtZDJteDFpY3lwIn0.3Exi_QhPZuuxHRcBXmpo5A'
+	}).addTo(cosmos.map);
+	cosmos.getWMSlayers();
+	return;
+}
+cosmos.updateMap = function(stationData)
 {
 	var markerIcon = L.icon({
 		iconUrl: '/includes/images/redicon.png',
@@ -184,15 +261,8 @@ cosmos.drawMap = function(stationData)
 		popupAnchor: [-7, -12]
 	});
 	var stationCoords = [stationData.latitude,stationData.longitude];
-	cosmos.map = L.map('mapid');
-	cosmos.osMap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		id: 'mapbox.streets',
-		accessToken: 'pk.eyJ1IjoiZ2VtbWF2bmFzaCIsImEiOiJjamdwNjR5YzMwZTc5MndtZDJteDFpY3lwIn0.3Exi_QhPZuuxHRcBXmpo5A'
-	}).addTo(cosmos.map);
 	cosmos.map.setView(stationCoords, 13);
 	var marker = L.marker(stationCoords, {title:stationData["site-name"], icon:markerIcon}).addTo(cosmos.map);
-	cosmos.getWMSlayers();
 	return;
 }
 cosmos.getWMSlayers = function()
@@ -248,7 +318,6 @@ cosmos.drawGraphs = function()
 		//or the date range
 		else if(cosmos.graphParams.startDate != 0 && cosmos.graphParams.endDate != 0 && cosmos.graphParams.urlType === 'months')
 		{
-			
 			graphSrc+='&date='+cosmos.graphParams.startDate+'/'+cosmos.graphParams.endDate;
 			hasParams = true;
 		}
@@ -260,5 +329,15 @@ cosmos.drawGraphs = function()
 		}
 	}
 	return;
+}
+cosmos.outputImages = function()
+{
+	$('#mapContainer').append('<div class="leftCol33"><figure class="centred"><a href="images/maps/AirPhoto_%20'+cosmos.station+'.png" title="'+cosmos.station+' satellite photo" data-featherlight="image"><img src="images/maps/AirPhoto_%20'+cosmos.station+'.png" alt="'+cosmos.station+' satellite photo" /></a><figcaption>'+cosmos.station+' satellite photo</figcaption></figure></div>');
+	$('#mapContainer').append('<div class="leftCol33"><figure class="centred"><a href="images/maps/LCM_%20'+cosmos.station+'.png" title="'+cosmos.station+' LCM" data-featherlight="image"><img src="images/maps/LCM_%20'+cosmos.station+'.png" alt="'+cosmos.station+' LCM" /></a><figcaption>'+cosmos.station+' LCM</figcaption></figure></div>');
+	$('#mapContainer').append('<div class="leftCol33"><figure class="centred"><a href="images/maps/StreetView_%20'+cosmos.station+'.png" title="'+cosmos.station+' street view" data-featherlight="image"><img src="images/maps/StreetView_%20'+cosmos.station+'.png" alt="'+cosmos.station+' street view" /></a><figcaption>'+cosmos.station+' street view</figcaption></figure></div>');
+	$('#stationPhotos').append('<div class="leftCol33"><figure class="centred"><a href="images/photos/photo1_'+cosmos.station+'.jpg" title="'+cosmos.station+' photo 1" data-featherlight="image"><img src="images/photos/photo1_'+cosmos.station+'.jpg" alt="'+cosmos.station+' photo 1" /></a><figcaption>'+cosmos.station+' photo 1</figcaption></figure></div>');
+	$('#stationPhotos').append('<div class="leftCol33"><figure class="centred"><a href="images/photos/photo1_'+cosmos.station+'.jpg" title="'+cosmos.station+' photo 2" data-featherlight="image"><img src="images/photos/photo2_'+cosmos.station+'.jpg" alt="'+cosmos.station+' photo 2" /></a><figcaption>'+cosmos.station+' photo 2</figcaption></figure></div>');
+	$('#stationPhotos').append('<div class="leftCol33"><figure class="centred"><a href="images/photos/pheno_'+cosmos.station+'.jpg" title="'+cosmos.station+' phenocam" data-featherlight="image"><img src="images/photos/pheno_'+cosmos.station+'.jpg" alt="'+cosmos.station+' phenocam" /></a><figcaption>'+cosmos.station+' phenocam</figcaption></figure></div>');
+	
 }
 $(document).ready(cosmos.init);
